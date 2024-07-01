c41315050a24c35bd30f23dc9733d145368352d9
Aza-0505 <kobakhidzeaza@gmail.com>
2024-07-01 04:15:37 +0400

    homework

---
+++ script.js
@@ -0,0 +1,72 @@
+1.//variables
+let number=100;
+if (number<50); {
+    console.log (<50);
+} if (number >20); {
+    console.log (>20);
+} else {
+    console.log ('error');
+}
+2.// conditional statemant
+let userName = 'mariam';
+if(userName='mariam');{
+    HTMLFormControlsCollection.log(true);
+} else {
+    console.log('error');
+}
+3.//switch case
+let userName='mariam';
+switch(userName='mariam') {
+    case 'value1':  // if (userName === 'mariam'){
+    console.log ('true');}
+    else { console.log('error');}
+      ]
+  
+  4.//loop array
+  let numberArray[5,6,7,8,9,...100];
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+  } for (let i=0; i<95; i++); {
+    console.log(numbers);
+  }
+ 5. // array
+  let array1= [1, 2, 4, -. 1, -3, 8, 7, 0, -5, 18, 12];
+ for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+ } if (element>0); {
+    console.log(element);
+ }
+ if (element<10);{
+    console.log(element);
+ }
+ 6.// usersinfo
+ let user2={
+    name:'anna',
+    age: 20,
+    sudentStatus:'active'
+
+ } if (userAge>18 && studentStatus='active');{
+    console.log ('hello user');
+ } if (userName='levani'); {
+    console.log ('hello levani');
+ } if (studentStatus='active' // userAge<25);{
+    console.log ('hello anna');
+} else {
+    console.log ('error');
+}
+7.//
+let array6 = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+if (element question %2 ===0); {
+    console.log (element);
+} 
+8. // 
+let users = [
+    {username: 'giorgi', status: false},
+    {username: 'levani', status: false},
+    {username: 'anna', status: true}
+    Dot Notification
+    console.log (userinfo[true]);
+
+
